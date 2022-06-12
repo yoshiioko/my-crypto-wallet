ed95f8fe8394b281ee7511edd4c67e3466020faf
yoshiioko <acardoza@pdx.edu>
2022-06-11 22:05:47 -0700

    script that allows a wallet to send Ether to another

---
+++ 03_send.js
@@ -0,0 +1,33 @@
+/**
+ * @fileoverview Allows the user to restore their wallet using a mnemonic seed phrase
+ * Run CMD: node 03_send.js "receiverAddress" "ammount"
+ */
+
+const { getDefaultProvider, Wallet, utils } = require("ethers");
+const { readFileSync } = require("fs");
+
+async function main(_receiverAddress, _ethAmount) {
+  const network = "rinkeby";
+  const provider = getDefaultProvider(network);
+
+  const accountRawData = readFileSync("account 1.json", "utf8");
+  const accountData = JSON.parse(accountRawData);
+
+  const privateKey = Object.values(accountData.privateKey);
+
+  const signer = new Wallet(privateKey, provider);
+
+  const transaction = await signer.sendTransaction({
+    to: _receiverAddress,
+    value: utils.parseEther(_ethAmount),
+  });
+
+  console.log(transaction);
+}
+
+main(process.argv[2], process.argv[3])
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
